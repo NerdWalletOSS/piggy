88c91f215d5e2637e42fb72f68365e93e4cfe755
Martin Cotta <mcotta@nerdwallet.com>
2022-03-14 22:03:37 -0700

    check if directory exist before patching

--- app/script/patch-modules.sh
+++ app/script/patch-modules.sh
@@ -1,15 +1,22 @@
 #!/bin/bash
-pushd node_modules/adbkit
-rm src 2> /dev/null
-ln -s lib src 2> /dev/null
-popd
 
-pushd node_modules/adbkit-logcat
-rm src 2> /dev/null
-ln -s lib src 2> /dev/null
-popd
+if [ -d "node_modules/adbkit" ]; then
+  pushd node_modules/adbkit
+  rm src 2> /dev/null
+  ln -s lib src 2> /dev/null
+  popd
+fi
 
-pushd node_modules/adbkit-monkey
-rm src 2> /dev/null
-ln -s lib src 2> /dev/null
-popd
+if [ -d "node_modules/adbkit-logcat" ]; then
+  pushd node_modules/adbkit-logcat
+  rm src 2> /dev/null
+  ln -s lib src 2> /dev/null
+  popd
+fi
+
+if [ -d "node_modules/adbkit-monkey" ]; then
+  pushd node_modules/adbkit-monkey
+  rm src 2> /dev/null
+  ln -s lib src 2> /dev/null
+  popd
+fi
