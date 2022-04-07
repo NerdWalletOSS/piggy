f325ad8b2846c6cbf7a16aa0ab867180cb5ca04e
Martin Cotta <mcotta@nerdwallet.com>
2022-04-07 09:30:40 -0700

    Use notarytool and API key credentials for notarization (#4)

--- app/script/notarize.js
+++ app/script/notarize.js
@@ -7,42 +7,40 @@ const {
 module.exports = async (context) => {
   const { electronPlatformName, appOutDir } = context;
   if (electronPlatformName !== 'darwin') {
-    console.error('You can build MacOS .app only on MacOS.');
+    console.warn('You can build MacOS .app only on MacOS.');
     return;
   }
+
   const appName = context.packager.appInfo.productFilename;
   const appPath = path.join(context.appOutDir, `${appName}.app`);
   if (appPath.includes('/dist/win')) {
-    console.log(`skipping notarization for ${appPath}`);
+    console.warn(`skipping notarization for ${appPath}`);
     return;
   }
 
-  if (
-    !process.env.ASC_EMAIL ||
-    !process.env.ASC_PASSWORD ||
-    !process.env.ASC_PROVIDER
-  ) {
-    console.log(
-      `skipping notarization for ${appName}, required credentials not found.`
-    );
+  const {
+    APPLE_API_KEY: appleApiKey,
+    APPLE_API_KEY_ID: appleApiKeyId,
+    APPLE_API_ISSUER: appleApiIssuer,
+  } = process.env;
+
+  if (!appleApiKey || !appleApiKeyId || !appleApiIssuer) {
+    console.warn('skipping notarization, required credentials not found');
     return;
   }
-
-  console.log(`Notarizing ${appId} (${appPath})`);
 
   try {
+    console.warn(`Notarizing ${appId} (${appPath})`);
     await notarize({
+      tool: 'notarytool',
       appBundleId: appId,
       appPath: `${appOutDir}/${appName}.app`,
-      appleId: process.env.ASC_EMAIL,
-      appleIdPassword: process.env.ASC_PASSWORD,
-      ascProvider: process.env.ASC_PROVIDER,
+      appleApiKey,
+      appleApiKeyId,
+      appleApiIssuer,
     });
-
     console.log(`Done notarizing ${appId}`);
-  } catch (error) {
-    console.error(
-      'Failed! Please ensure valid credentials are specified via environment variables.'
-    );
+  } catch {
+    console.error('Failed! Please ensure valid credentials are specified via environment variables.');
   }
 };
