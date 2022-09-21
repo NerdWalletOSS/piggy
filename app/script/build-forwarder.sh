21d40981de90690c1f71daae9edfc983b6b43973
casey langen <clangen@nerdwallet.com>
2022-09-21 13:01:46 -0700

    Don't hardcode the development team for forwarder; instead, sign locally unless there is a APPLE_DEVELOPMENT_TEAM env var set.

--- app/script/build-forwarder.sh
+++ app/script/build-forwarder.sh
@@ -2,6 +2,11 @@
 
 set -x
 
+EXTRA_ARGS=
+if [[ ! -z $APPLE_DEVELOPMENT_TEAM ]]; then
+  EXTRA_ARGS="DEVELOPMENT_TEAM=${APPLE_DEVELOPMENT_TEAM}"
+fi
+
 if [ `uname` = "Darwin" ]; then
   pushd extras/forwarder
   bundle install
@@ -12,7 +17,8 @@ if [ `uname` = "Darwin" ]; then
     -configuration Release \
     -scheme forwarder \
     -derivedDataPath extras/forwarder/build/DerivedData \
-    -verbose
+    -verbose \
+    ${EXTRA_ARGS}
   mkdir -p resources/mac/bin 2> /dev/null
   cp extras/forwarder/build/DerivedData/Build/Products/Release/forwarder resources/mac/bin/
 fi
