e60b07e40a9efb32caaf3599c671df7a1660ed3e
casey langen <clangen@nerdwallet.com>
2023-02-01 15:42:25 -0800

    Update Redux plugin to use `serverProxy.emitWs`

--- app/src/App/Plugins/Standard/Redux/Redux.js
+++ app/src/App/Plugins/Standard/Redux/Redux.js
@@ -15,8 +15,10 @@ import { indexChildren } from '@lib/search';
 import styles from './ReduxStyles';
 import { ReduxStateSliceItem } from './ReduxStateSliceItem';
 
-const SET_STATE_SUBSCRIPTIONS_PATHS_MESSAGE = '/stateSubscriptions/setPaths';
-const UPDATE_MESSAGE = '/stateSubscriptions/update';
+const SERVER_WS_MESSAGE = {
+  SEND_PATHS_TO_CLIENT_MESSAGE: '/stateSubscriptions/setPaths',
+  DATA_UPDATED_FROM_CLIENT_MESSAGE: '/stateSubscriptions/update',
+};
 
 const KEY_MAP = {
   NEW_SUBSCRIPTION: 'command+n',
@@ -37,7 +39,7 @@ export default class StateSubscriptions extends PureComponent {
   }
 
   // Called when a subscription updates
-  handleUpdateMessage = (event, message) => {
+  handleUpdateMessage = (_event, message) => {
     const { subscriptions } = this.state;
     const updatedSubscriptions = message.data;
     const mergedStateSubscriptions = { ...subscriptions };
@@ -65,11 +67,17 @@ export default class StateSubscriptions extends PureComponent {
   };
 
   componentDidMount() {
-    serverProxy.onWs(UPDATE_MESSAGE, this.handleUpdateMessage);
+    serverProxy.onWs(
+      SERVER_WS_MESSAGE.DATA_UPDATED_FROM_CLIENT_MESSAGE,
+      this.handleUpdateMessage
+    );
   }
 
   componentWillUnmount() {
-    serverProxy.offWs(UPDATE_MESSAGE, this.handleUpdateMessage);
+    serverProxy.offWs(
+      SERVER_WS_MESSAGE.DATA_UPDATED_FROM_CLIENT_MESSAGE,
+      this.handleUpdateMessage
+    );
   }
 
   toggleModal = () =>
@@ -86,14 +94,9 @@ export default class StateSubscriptions extends PureComponent {
 
   handleAddSubscription = () => {
     const { subscriptions, newSubscriptionPath } = this.state;
-
-    global.ipc.events.emit('/ws/send', {
-      name: SET_STATE_SUBSCRIPTIONS_PATHS_MESSAGE,
-      data: {
-        paths: [...Object.keys(subscriptions), newSubscriptionPath],
-      },
+    serverProxy.emitWs(SERVER_WS_MESSAGE.SEND_PATHS_TO_CLIENT_MESSAGE, {
+      paths: [...Object.keys(subscriptions), newSubscriptionPath],
     });
-
     this.toggleModal();
     this.clearNewSubscription();
   };
@@ -102,11 +105,8 @@ export default class StateSubscriptions extends PureComponent {
     const { subscriptions } = this.state;
     const remainingSubscriptions = _.omit(subscriptions, [subscriptionKey]);
     this.setState({ subscriptions: remainingSubscriptions }, () => {
-      global.ipc.events.emit('/ws/send', {
-        name: SET_STATE_SUBSCRIPTIONS_PATHS_MESSAGE,
-        data: {
-          paths: Object.keys(remainingSubscriptions),
-        },
+      serverProxy.emitWs(SERVER_WS_MESSAGE.SEND_PATHS_TO_CLIENT_MESSAGE, {
+        paths: Object.keys(remainingSubscriptions),
       });
     });
   };
